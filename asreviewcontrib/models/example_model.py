00c3377d01619da40543079702b98a589eed924d
Jelle Teijema <jelle@teije.ma>
2025-05-08 13:18:29 +0200

    Clean up code for 2.0

--- asreviewcontrib/models/example_model.py
+++ asreviewcontrib/models/example_model.py
@@ -1,25 +1,24 @@
+__all__ = ["NaiveBayesDefaultParamsModel"]
+
 from sklearn.naive_bayes import MultinomialNB
 
-from asreview.models.classifiers.base import BaseTrainClassifier
-
-
-class NaiveBayesDefaultParamsModel(BaseTrainClassifier):
+class NaiveBayesDefaultParamsModel(MultinomialNB):
     """Naive Bayes classifier
 
     The Naive Bayes classifier with the default SKLearn parameters.
     """
 
     name = "nb_example"
+    label = "The Naive Bayes classifier"
 
     def __init__(self):
-
         super().__init__()
         self._model = MultinomialNB()
 
         def fit(self, X, y):
             """Fit the model to the data."""
-            return self._model.fit(X, y)
+            return self.fit(X, y)
 
         def predict_proba(self, X):
             """Get the inclusion probability for each sample."""
-            return self._model.predict_proba(X)
+            return self.predict_proba(X)
